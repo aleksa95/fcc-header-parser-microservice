70a8eaaba3c2df61d2697bd1e3976ed703619710
Stankovic <aleksa.stankovic@berkeley.edu>
2017-01-29 11:11:08 +0100

    done

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/get-info', function(req, res) {
+    var language = req.headers["accept-language"];
+    language = language.substring(0, language.indexOf(','));
+    var rgx = /\(([^)]+)\)/;
+    var match = req.headers['user-agent'].match(rgx);
+    var software = match && match[1];
+
+    res.json(
+         {
+            ipaddress: req.headers["host"],
+            language: language,
+            software: software
+         }
+     )
+});
+
+module.exports = router;
